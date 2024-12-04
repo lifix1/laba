cdefb9a55813fb79726ddb4621e74eaf7d45b333
Nostraw <Nostraw1@yandex.ru>
2024-12-04 23:19:55 +0300

    Добавлены тестовые данные

--- labacode/main.cpp
+++ labacode/main.cpp
@@ -2,7 +2,11 @@
 
 using namespace std;
 
-int main() {
-	cout << "Вариант 9 'Телефонные разговоры' Петров Станислав 24ПИнж" << endl;
-	return 0;
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #9. phone conversation\n";
+    cout << "Author: Petrov Stanislav\n";
+    cout << "Group: 24PInzh\n";
+    return 0;
 }
